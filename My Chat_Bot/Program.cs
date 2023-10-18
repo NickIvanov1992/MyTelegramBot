d2fbf5718a477ce6cfe5a3d4f77ac1ed019952e0
NickIvanov1992 <nikolas-bmx@mail.ru>
2023-10-19 02:25:52 +0400

    new telegramBot

---
+++ My Chat_Bot/Program.cs
@@ -0,0 +1,38 @@
+﻿var lines = File.ReadAllLines("data.txt");
+var questions = lines.Select(s => s.Split("|")).Select(s => (s[0], s[1])).ToList();
+var random = new Random();
+var count = questions.Count;
+var score = 0;
+
+
+while (true)
+{
+    var index = random.Next(count - 1);
+    var question = questions[index];
+
+    var opened = 0;
+    while (opened < question.Item2.Length)
+    {
+        Console.WriteLine($"{question.Item1}: {question.Item2.Length} букв");
+        var answer = question.Item2.Substring(0, opened).PadRight(question.Item2.Length, '*');
+        Console.WriteLine(answer);
+        var tryAnswer = Console.ReadLine().ToLower().Replace('ё','е');
+        if (tryAnswer == question.Item2)
+        {
+            score++;
+            Console.WriteLine("Верно!");
+            Console.WriteLine($" У вас{score} очков");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Не верно!");
+            opened++;
+        }
+    }
+     if(opened == question.Item2.Length)
+    {
+        Console.WriteLine($"Правильный ответ: {question.Item2}");
+    }
+}
+
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using static System.Formats.Asn1.AsnWriter;
+using File = System.IO.File;
+
+using CancellationTokenSource cts = new();
+
+
+var quiz = new Quiz("data.txt");
+var token = "6625605730:AAG2mpJwAwXLCFeC_gyY8EUNnrKMesLbSRM";
+var bot = new TelegramBotClient(token);
+var States = new Dictionary<long,QuestionState>();
+var UserScores = new Dictionary<long, int>();
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
+
+    bot.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+Console.ReadKey();
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    if (!States.TryGetValue(chatId, out var state))
+    {
+        state = new QuestionState();
+        States[chatId] = state;
+    }
+    if(state.CurrentItem == null)
+    {
+        state.CurrentItem = quiz.NextQuestion();
+    }
+    
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+   
+
+
+    var question = state.CurrentItem;
+    var tryAnswer = messageText.ToLower().Replace('ё', 'е');
+    if (tryAnswer == question.Answer)
+    {
+        //score++;
+        await botClient.SendTextMessageAsync(chatId: chatId,
+        text: "Правильно!",
+        cancellationToken: cancellationToken);
+
+        Console.WriteLine("Верно!");
+        if(UserScores.ContainsKey(message.From.Id))
+        {
+            UserScores[message.From.Id]++;
+        }
+        else
+        {
+            UserScores[message.From.Id] = 1;
+        }
+        state.CurrentItem = quiz.NextQuestion();
+        //Console.WriteLine($" У вас{score} очков");
+        await botClient.SendTextMessageAsync(chatId: chatId,
+        text: $"У вас {UserScores} очков",
+        cancellationToken: cancellationToken);
+
+
+    }
+    else
+    {
+        state.Opened++;
+        if (state.IsEnd)
+        {
+        await botClient.SendTextMessageAsync(chatId: chatId,
+        text: $"Правильный ответ: {question.Answer}",
+        cancellationToken: cancellationToken);
+
+            Console.WriteLine($"Правильный ответ: {question.Answer}");
+        }
+        await botClient.SendTextMessageAsync(chatId: chatId,
+        text: "Не правильно!",
+        cancellationToken: cancellationToken);
+
+        Console.WriteLine("Не верно!");
+        //opened++;
+    }
+    await botClient.SendTextMessageAsync(chatId: chatId,
+        text: state.DisplayQuestion,
+        cancellationToken: cancellationToken);
+    // Echo received message text
+    void NewRound(long chatId)
+    {
+
+    }
+
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+public class Quiz
+{
+    List<QuestionItem> Questions { get; set; }
+
+    private Random random;
+    private int count;
+    public Quiz(string path = "data.txt")
+    {
+        var lines = File.ReadAllLines(path);
+        Questions = lines.Select(s => s.Split("|")).Select(s => new QuestionItem
+        {
+            Question = s[0],
+            Answer = s[1]
+        }).ToList();
+        random = new Random();
+        count = Questions.Count;
+    }
+
+    public QuestionItem NextQuestion()
+    {
+        var index = random.Next(count - 1);
+        var question = Questions[index];
+        return question;
+    }
+    
+}
+public class QuestionItem
+{
+    public string Question { get; set; }
+    public string Answer { get; set; }
+}
+
+public class QuestionState
+{
+    public QuestionItem CurrentItem { get; set; }
+    public int Opened { get; set; } 
+    public string AnswerHint => CurrentItem.Answer.Substring(0, Opened).PadRight(CurrentItem.Answer.Length, '*');
+    public string DisplayQuestion => $"{CurrentItem.Question} :  {CurrentItem.Answer.Length} букв \n" +
+        $" {AnswerHint}";
+    public bool IsEnd => Opened == CurrentItem.Answer.Length;
+}
