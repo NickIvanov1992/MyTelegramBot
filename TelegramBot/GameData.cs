e62c599f8544d783f628bb1112311bec0537561c
NickIvanov1992 <nikolas-bmx@mail.ru>
2023-10-26 00:44:54 +0400

    Refactoring2

--- TelegramBot/GameData.cs
+++ TelegramBot/GameData.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -10,23 +11,30 @@ namespace TelegramBot
 {
     public class GameData
     {
-        private readonly string  StateFileName = "state.json";
-        private readonly string  ScoreFileName = "score.json";
-
-        public void LoadGame(ref Dictionary<long, QuestionState>?  States, ref Dictionary<long, int>? UserScores)
+        private readonly string StateFileName = "state.json";
+        private readonly string ScoreFileName = "score.json";
+        User user = new();
+        Quiz quiz = new("data.txt");
+        public GameData(User user, Quiz quiz)
+        {
+            this.user = user;
+            this.quiz = quiz;
+        }
+        public void LoadGame()
         {
             if (File.Exists(StateFileName))
             {
                 var json = File.ReadAllText(StateFileName);
-                States = JsonConvert.DeserializeObject<Dictionary<long, QuestionState>>(json);
+                user.States = JsonConvert.DeserializeObject<Dictionary<long, QuestionState>>(json);
             }
             if (File.Exists(ScoreFileName))
             {
                 var json = File.ReadAllText(ScoreFileName);
-                UserScores = JsonConvert.DeserializeObject<Dictionary<long, int>>(json);
+                user.UserScores = JsonConvert.DeserializeObject<Dictionary<long, int>>(json);
             }
         }
-        public void SaveGame( Dictionary<long, QuestionState> States, Dictionary<long, int> UserScores)
+
+        public void SaveGame(Dictionary<long, QuestionState> States, Dictionary<long, int> UserScores)
         {
             var stateJson = JsonConvert.SerializeObject(States);
             File.WriteAllText(StateFileName, stateJson);
@@ -34,5 +42,92 @@ namespace TelegramBot
             var scoreJson = JsonConvert.SerializeObject(UserScores);
             File.WriteAllText(ScoreFileName, scoreJson);
         }
+        public void SaveState(Dictionary<long, QuestionState> States)
+        {
+            var stateJson = JsonConvert.SerializeObject(States);
+            File.WriteAllText(StateFileName, stateJson);
+        }
+        public void SaveScores(Dictionary<long, int> UserScores, long fromId)
+        {
+            var scoreJson = JsonConvert.SerializeObject(UserScores);
+            File.WriteAllText(ScoreFileName, scoreJson);
+
+        }
+        public void CheckUserState(long chatId, long fromId)
+        {
+            if (!user.States.TryGetValue(chatId, out var state))
+            {
+                state = new QuestionState();
+                user.States[chatId] = state;
+                user.UserScores[fromId] = 5;
+            }
+            if (state.CurrentItem == null)
+            {
+                state.CurrentItem = quiz.NextQuestion();
+            }
+        }
+        public string CheckState(QuestionItem item, string answer, long chatId, long fromId)
+        {
+            if (answer == item.Answer && user.UserScores[fromId] > 0)
+            {
+                user.States[chatId].Opened = 0;
+                Console.WriteLine("Верно!");
+
+                user.UserScores[fromId]++;
+
+                user.States[chatId].CurrentItem = quiz.NextQuestion();
+
+
+                return $"Правильно! У вас {user.UserScores[fromId]} очков";
+            }
+            else if (answer != item.Answer && user.UserScores[fromId] > 1)
+            {
+                user.States[chatId].Opened++;
+                user.UserScores[fromId]--;
+                if (user.States[chatId].IsEnd)
+                {
+                    user.States[chatId].Opened = 0;
+                    user.States[chatId].CurrentItem = quiz.NextQuestion();
+                    Console.WriteLine($"Не верно!  Правильный ответ: {item.Answer}");
+                    return $"Не верно! Правильный ответ: {item.Answer} ";
+                }
+                Console.WriteLine("Не верно!");
+                return $"Не верно! \n" +
+                    $"Осталось {user.UserScores[fromId]} очков";
+            }
+            else if (answer == "start")
+            {
+                user.UserScores[fromId] = 5;
+                user.States[chatId].CurrentItem = quiz.NextQuestion();
+
+                return $"Игра началась! \n" +
+                    $" У вас {user.UserScores[fromId]} очков";
+            }
+            else /*if (user.UserScores[fromId] == 0)*/
+            {
+                user.States[chatId].Opened = 0;
+                user.UserScores[fromId] = 0;
+
+                Console.WriteLine($"Игра окончена. У вас 0 очков. \n " +
+                                    $"Правильный ответ: {item.Answer}");
+
+                return $"Игра окончена. У вас 0 очков. \n " +
+                            $"Правильный ответ: {item.Answer} \n" +
+                            $"РЕКОРДЫ:\n" +
+                            $" {GetGameRecords(user)}";
+            }
+        }
+        public string GetGameRecords(User users)
+        {
+
+            List<string> scores = new List<string>();
+            foreach (var user in users.UserScores)
+            {
+                scores.Add($"Имя: {user.Key} Очков: {user.Value}");
+            }
+            var result = String.Join(",", scores.ToArray());
+            return result;
+        }
     }
+
 }
---
+++ TelegramBot/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    public class User
+    {
+       public Dictionary<long, QuestionState> States = new Dictionary<long, QuestionState>();
+        public Dictionary<long, int> UserScores = new Dictionary<long, int>();
+        //public User(Dictionary<long, QuestionState> states, Dictionary<long, int> userScores)
+        //{
+        //    States = states;
+        //    UserScores = userScores;
+        //}
+    }
+}
